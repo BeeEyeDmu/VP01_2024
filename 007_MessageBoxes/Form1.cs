5764b4e25a14a6eacf85eddd55829e53936fb31d
BeeEye Dmu <bikang@konyang.ac.kr>
2024-04-09 17:08:24 +0900

    Add files via upload

---
+++ 007_MessageBoxes/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _007_MessageBoxes
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      //MessageBox.Show("안녕");
+      //MessageBox.Show("안녕하세요", "Hello");
+      //MessageBox.Show("두개의 버튼",
+      //  "Question",
+      //  MessageBoxButtons.YesNo);
+      //MessageBox.Show("세개의 버튼과 물음표", "Question",
+      //  MessageBoxButtons.YesNoCancel,
+      //  MessageBoxIcon.Question);
+
+      DialogResult result = MessageBox.Show("세개의 버튼과 물음표", "Question",
+        MessageBoxButtons.YesNoCancel,
+        MessageBoxIcon.Question,
+        MessageBoxDefaultButton.Button2);
+
+      if(result == DialogResult.Yes) { }
+      else if(result == DialogResult.No) { }
+
+
+      // <Ctrl-KC> : Comment로 만들기
+      // <Ctrl-KU> : UnComment로 만들기
+
+      //MessageBox.Show("느낌표와 알람","Title",
+      //  MessageBoxButtons.OK,
+      //  MessageBoxIcon.Exclamation);
+    }
+  }
+}
---
+++ 010_RadioButton/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _010_RadioButton
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      string result = "";
+
+      if(rbKorea.Checked == true) {
+        result += "국적 : 대한민국\n";
+      } else if (rbChina.Checked == true) {
+        result += "국적 : 중국\n";
+      } else if (rbJapan.Checked == true) {
+        result += "국적 : 일본\n";
+      } else if (rbOthers.Checked == true) {
+        result += "국적 : 그 외의 국가\n";
+      }
+
+      if (rbMale.Checked == true)
+        result += "성별 : 남성";
+      else if(rbFemale.Checked == true) 
+        result += "성별 : 여성";
+
+      MessageBox.Show(result, "국적/성별");
+    }    
+  }
+}
