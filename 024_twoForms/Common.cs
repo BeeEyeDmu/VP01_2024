bd4461a5ae87c0aec6812d820ae0a6d1abadbe3c
BeeEye Dmu <bikang@konyang.ac.kr>
2024-04-29 11:48:43 +0900

    Add files via upload

---
+++ 024_twoForms/Common.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024_twoForms
+{
+  public static class Common
+  {
+    // 필드와 속성 중 하나만 사용한다
+    public static string str = ""; // 필드
+    public static string Str { get; set; } // 속성(property)
+  }
+}
