440398d3e38f2a5e2ebea0a226111faccb41f4ed
BeeEye Dmu <bikang@konyang.ac.kr>
2024-06-03 11:47:26 +0900

    Add files via upload

--- 033_EIS/MainWindow.xaml.cs
+++ 033_EIS/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
 namespace _033_EIS
 {
@@ -35,6 +24,7 @@ namespace _033_EIS
     {
       InitializeComponent();
       conn = new MySqlConnection(connStr);
+      DisplayDataGrid();
     }
 
     private void btnInsert_Click(object sender, RoutedEventArgs e)
@@ -69,30 +59,120 @@ namespace _033_EIS
         MessageBox.Show("추가 성공!");
 
       conn.Close();
+      DisplayDataGrid();
     }
 
     private void btnUpdate_Click(object sender, RoutedEventArgs e)
     {
+      conn.Open();
 
+      if (rbMale.IsChecked == true)
+        gender = "남성";
+      else
+        gender = "여성";
+
+      dateEnter = dpEnter.Text;
+      dateExit = dpExit.Text;
+
+      try
+      {
+        string sql = string.Format("UPDATE eis_table SET "
+          + " name='{0}', department='{1}',position='{2}',"
+          + "gender='{3}',date_enter='{4}',date_exit='{5}',"
+          + "contact='{6}',comment='{7}' WHERE eid={8}",
+          txtName.Text, cbDept.Text, cbPos.Text, gender, dateEnter, dateExit,
+          txtContact.Text, txtComment.Text, txtEid.Text);
+
+        MySqlCommand cmd = new MySqlCommand(sql, conn);
+        if (cmd.ExecuteNonQuery() == 1)
+          MessageBox.Show("수정 성공!");
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+
+      conn.Close();
+      DisplayDataGrid();
     }
 
     private void btnDelete_Click(object sender, RoutedEventArgs e)
     {
+      conn.Open();
 
+      try
+      {
+        string sql = string.Format("DELETE FROM eis_table WHERE eid={0}",
+          txtEid.Text);
+        MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+        if (cmd.ExecuteNonQuery() == 1)
+          MessageBox.Show("삭제 성공!");
+      }
+      catch(Exception ex) 
+      { 
+        MessageBox.Show(ex.ToString()); 
+      }
+
+      conn.Close();
+      DisplayDataGrid();
     }
 
     private void btnLoadData_Click(object sender, RoutedEventArgs e)
     {
+      DisplayDataGrid();      
+    }
+
+    private void DisplayDataGrid()
+    {
       conn.Open();
 
       string sql = "SELECT * FROM eis_table";
-      
-      MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
-      DataSet ds = new DataSet();
-      da.Fill(ds);
-      dataGrid.ItemsSource = ds.Tables[0].DefaultView;
+
+      try
+      {
+        MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        dataGrid.ItemsSource = ds.Tables[0].DefaultView;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
 
       conn.Close();
+    }
+
+    private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      DataGrid dg = sender as DataGrid;
+      DataRowView rowView = dg.SelectedItem as DataRowView;
+
+      if (rowView == null) {
+        return;
+      }
+
+      txtEid.Text = rowView.Row[0].ToString();
+      txtName.Text = rowView.Row[1].ToString();
+      cbDept.Text = rowView.Row[2].ToString();
+      cbPos.Text = rowView.Row[3].ToString();
+
+      if (rowView.Row[4].ToString() == "남성")
+      {
+        rbMale.IsChecked = true;
+        rbFemale.IsChecked = false;
+      }
+      else
+      {
+        rbMale.IsChecked = false;
+        rbFemale.IsChecked = true;
+      }
+
+      dpEnter.Text = rowView.Row[5].ToString();
+      dpExit.Text = rowView.Row[6].ToString();
+      txtContact.Text = rowView.Row[7].ToString();
+      txtComment.Text = rowView.Row[8].ToString();
     }
   }
 }
